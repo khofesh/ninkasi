e8f9ea7305682884650bdcee9a59b1a59a891f9e
fahmi ahmad <fahmiahmad@tutanota.com>
2021-12-22 09:06:32 +0700

    parse let statements

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,8 @@
 // ast = abstract syntax tree
 package ast
 
+import "github.com/khofesh/ninkasi/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -25,4 +27,25 @@ func (p *Program) TokenLiteral() string {
 	} else {
 		return ""
 	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
