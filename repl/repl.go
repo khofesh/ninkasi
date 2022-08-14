af25bd088380776d1d2ee8cdbed9f539d1916433
fahmi ahmad <fahmiahmad@tutanota.com>
2022-08-14 22:09:48 +0700

    evaluate program

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/khofesh/ninkasi/evaluator"
 	"github.com/khofesh/ninkasi/lexer"
 	"github.com/khofesh/ninkasi/parser"
 )
@@ -100,8 +101,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
