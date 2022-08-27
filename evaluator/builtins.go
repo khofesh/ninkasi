fb16e5d7629c4f87951fd8a0bd502a58f6974c91
fahmi ahmad <fahmiahmad@tutanota.com>
2022-08-27 20:04:57 +0700

    add builtin print and comment out logging

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/khofesh/ninkasi/object"
+import (
+	"fmt"
+
+	"github.com/khofesh/ninkasi/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -95,6 +99,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"print": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
